d7182c8fcfb658ff936eb0e78641765e9131625a
Allison Dulin <allison.dulin@gmail.com>
2021-04-04 19:33:32 -0400

    implement type checking for add

--- math.py
+++ math.py
@@ -1,7 +1,9 @@
 # I need to implement this
 def add(x,y):
-    return x+y
- 
+    if (type(x)==int and type(y==int)):
+        return x+y
+    else:
+        return None
  #Bob needs to implement this
  def mult(x,y):
     return None
